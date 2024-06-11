1ffe648c45354f860bd7320e4dacfb5ca5ff071e
gabrielfogaca <gabriel.fogaca@unijui.edu.br>
2024-06-10 23:10:33 -0300

    login e cadastro

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCtmBD3roikkOJcZgn1OKypuTbbGy3Rzqc',
+  authDomain: 'controle-de-gastos-3f694.firebaseapp.com',
+  projectId: 'controle-de-gastos-3f694',
+  storageBucket: 'controle-de-gastos-3f694.appspot.com',
+  messagingSenderId: '86246335869',
+  appId: '1:86246335869:web:06825b65e46769386f5814',
+  measurementId: 'G-P4VSYWLM20',
+};
+firebase.initializeApp(firebaseConfig);
---
+++ js/login.js
@@ -0,0 +1,68 @@
+function onChangeEmail() {
+  toggleEmailErrors();
+}
+
+function onChangePassword() {
+  togglePasswordErrors();
+}
+
+function login() {
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(form.email().value, form.password().value)
+    .then((response) => {
+      window.location.href = 'inicio.html';
+    })
+    .catch((error) => {
+      alert(getErrorMessage(error));
+    });
+}
+
+function getErrorMessage(error) {
+  if (error.code == 'auth/internal-error') {
+    return 'Usuário ou senha incorretos';
+  }
+  return error.message;
+}
+
+function register() {
+  window.location.href = 'cadastro.html';
+}
+
+function toggleEmailErrors() {
+  const email = form.email().value;
+}
+
+function togglePasswordErrors() {
+  const password = form.password().value;
+}
+
+// function toggleButtonsDisable() {
+//   const emailValid = isEmailValid();
+//   form.recoverPasswordButton().disabled = !emailValid;
+
+//   const passwordValid = isPasswordValid();
+//   form.loginButton().disabled = !emailValid || !passwordValid;
+// }
+
+function isEmailValid() {
+  const email = form.email().value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
+}
+
+function isPasswordValid() {
+  return form.password().value ? true : false;
+}
+
+const form = {
+  email: () => document.getElementById('email'),
+  loginButton: () => document.getElementById('login-button'),
+  password: () => document.getElementById('password'),
+  passwordRequiredError: () =>
+    document.getElementById('password-required-error'),
+  recoverPasswordButton: () =>
+    document.getElementById('recover-password-button'),
+};
